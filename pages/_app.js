6a425d379571217469fb626df4d061e1767b8f6e
adamjmcgrath <adam.mcgrath@auth0.com>
2021-07-08 17:22:55 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  // If you've used `withAuth`, pageProps.user can pre-populate the hook
+  // if you haven't used `withAuth`, pageProps.user is undefined so the hook
+  // fetches the user from the API routes
+  const { user } = pageProps;
+
+  return (
+      <UserProvider user={user}>
+        <Component {...pageProps} />
+      </UserProvider>
+  );
 }
-
-export default MyApp
