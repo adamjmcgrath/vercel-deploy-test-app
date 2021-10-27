c610dbb787facdaf6542cfb026ad7a79a97644a2
adamjmcgrath <adam.mcgrath@auth0.com>
2021-10-27 11:59:45 +0100

    add cached api

--- pages/api/hello-cached.js
+++ pages/api/hello-cached.js
@@ -2,6 +2,6 @@ import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function handler(req, res) {
   const session = await getSession(req, res);
-  res.status(200).json(session.user);
   res.setHeader('Cache-Control', 's-maxage=86400');
+  res.status(200).json(session.user);
 })
