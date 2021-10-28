5b80a139096c1d1604acaea8d9f2ab4e70814ad9
adamjmcgrath <adam.mcgrath@auth0.com>
2021-10-28 15:15:47 +0100

    remove cached api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,6 @@ import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function handler(req, res) {
   const session = await getSession(req, res);
-  // res.setHeader('Cache-Control', 's-maxage=86400');
+  res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json(session.user);
 });
