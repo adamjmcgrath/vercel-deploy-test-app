e68e1c823522fb8f1b1bebe4201a0b01e39f5162
adamjmcgrath <adam.mcgrath@auth0.com>
2022-08-31 15:11:22 +0100

    remove route

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useUser } from "@auth0/nextjs-auth0";
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const { user, isLoading } = useUser(null);
+  const { user } = useUser(null);
 
   return (
     <div className={styles.container}>
@@ -21,8 +21,6 @@ export default function Home() {
           <a href="/auth-required"><button>Auth Required</button></a><br/>
           {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
           <a href="/auth-required-mw"><button>Auth Required (Middleware)</button></a><br/>
-          {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-          <a href="/auth-required-mw-no-user"><button>Auth Required (Middleware) no user</button></a><br/>
           {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
           { user && <a href="/api/auth/logout"><button>logout</button></a>}
         </p>
