8048f6d405cbdfaa1eb255752715a3a96fba72b2
adamjmcgrath <adam.mcgrath@auth0.com>
2023-02-07 10:07:47 -0400

    add mw helper back

--- middleware.js
+++ middleware.js
@@ -1,19 +1,6 @@
-import { NextResponse } from 'next/server';
-import { getSession } from '@auth0/nextjs-auth0/edge';
+import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 
-export async function middleware(req) {
-  const res = NextResponse.next();
-  const user = await getSession(req, res);
-
-  console.log('cookie', req.headers.get('cookie'));
-  const cookie = req.headers.get('cookie');
-
-  return new NextResponse(
-      JSON.stringify({ cookie, length: cookie?.length, user }),
-      { headers: { 'content-type': 'application/json' } }
-  )
-
-}
+export default withMiddlewareAuthRequired();
 
 export const config = {
   matcher: ['/auth-required-mw'],
