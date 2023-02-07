99b50f2b66be3d7eafc66ac328a8e02a8294ebad
adamjmcgrath <adam.mcgrath@auth0.com>
2023-02-07 10:34:19 -0400

    test 6

--- middleware.js
+++ middleware.js
@@ -1,19 +1,8 @@
-import { NextResponse } from 'next/server';
-import { getSession, withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 
 export async function middleware(req, ev) {
-  const res = NextResponse.next();
-  const user = await getSession(req, res);
-
-  console.log(await withMiddlewareAuthRequired()(req, ev));
-  console.log('cookie', req.headers.get('cookie'));
-  const cookie = req.headers.get('cookie');
-
-  return new NextResponse(
-      JSON.stringify({ cookie, length: cookie?.length, user }),
-      { headers: { 'content-type': 'application/json' } }
-  )
-
+  console.log(req.cookies.get('appSession.0')?.value);
+  return withMiddlewareAuthRequired()(req, ev);
 }
 
 export const config = {
