320736eeb17062745a46e6bcdbd6ecfc8a2a7f4b
adamjmcgrath <adam.mcgrath@auth0.com>
2023-02-07 09:57:23 -0400

    cookie test

--- middleware.js
+++ middleware.js
@@ -1,12 +1,17 @@
 import { NextResponse } from 'next/server';
+import { getSession } from '@auth0/nextjs-auth0/edge';
 
-export function middleware(req) {
-  const cookie = req.headers.get('cookie');
+export async function middleware(req) {
+  const res = NextResponse.next();
+  const user = await getSession(req, res);
+
+  console.log('cookie', req.headers.get('cookie'));
 
   return new NextResponse(
       JSON.stringify({ cookie, length: cookie?.length }),
       { headers: { 'content-type': 'application/json' } }
   )
+
 }
 
 export const config = {
