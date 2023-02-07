8dcfc918988c8a06536edca8e5c81dce02154913
adamjmcgrath <adam.mcgrath@auth0.com>
2023-02-07 10:15:41 -0400

    test 3

--- middleware.js
+++ middleware.js
@@ -1,7 +1,22 @@
-import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+import {NextResponse} from "next/server";
+import { withMiddlewareAuthRequired, getSession } from '@auth0/nextjs-auth0/edge';
 
-export default withMiddlewareAuthRequired();
+const auth0Middleware = withMiddlewareAuthRequired();
+
+export default async function middleware(req, ev) {
+  const { pathname } = req.nextUrl;
+
+  console.log(1, req.headers.get('cookie'));
+
+  if (pathname === '/auth-required-mw') {
+    return auth0Middleware(req, ev);
+  }
+  const user = await getSession(req, NextResponse.next())
+
+  console.log('user', user);
+  console.log(2, req.headers.get('cookie'));
+}
 
 export const config = {
-  matcher: ['/auth-required-mw'],
+  matcher: ['/auth-required-mw', '/check-cookie'],
 };
