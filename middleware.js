d3e47da4283b5bd3830e2be21d2bc9f1e3678edd
adamjmcgrath <adam.mcgrath@auth0.com>
2023-02-07 08:53:30 -0400

    update next and mw fn

--- middleware.js
+++ middleware.js
@@ -1,6 +1,17 @@
-import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+import { NextResponse } from 'next/server';
+import { getSession } from '@auth0/nextjs-auth0/edge';
 
-export default withMiddlewareAuthRequired();
+export async function middleware(req) {
+  const res = NextResponse.next();
+  const user = await getSession(req, res);
+
+  console.log('user', user);
+  console.log('cookie header', req.headers.get('cookie'));
+  console.log('cookies', JSON.stringify(req.cookies.getAll(), null, 2));
+
+  return res;
+
+}
 
 export const config = {
   matcher: ['/auth-required-mw'],
